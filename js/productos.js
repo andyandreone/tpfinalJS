ddde188a0588af55d92e978b95ce2aeed2ae5963
andyandreone <andy1.-@hotmail.com>
2021-05-23 20:47:28 -0300

    Se elimino console.log

--- js/productos.js
+++ js/productos.js
@@ -2,7 +2,6 @@
 const productos = [];
 $.ajaxSetup({async: false});
 $.getJSON("../data.json", function(result, estado){
-    console.log(estado)
     for (const producto of result) {
         productos.push(producto);
     }
