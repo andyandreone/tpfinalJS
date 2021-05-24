a8793fe86c1ae708902527909be632243a5ca619
andyandreone <andy1.-@hotmail.com>
2021-05-23 22:58:51 -0300

    console log status

--- js/productos.js
+++ js/productos.js
@@ -2,6 +2,7 @@
 const productos = [];
 $.ajaxSetup({async: false});
 $.getJSON("../data.json", function(result, estado){
+    console.log(status);
     for (const producto of result) {
         productos.push(producto);
     }
