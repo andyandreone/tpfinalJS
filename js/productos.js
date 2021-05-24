848381c6bab8ac28531e99012c013a7ec20d2dbe
andyandreone <andy1.-@hotmail.com>
2021-05-23 23:49:38 -0300

    moficacion metodo ajax

--- js/productos.js
+++ js/productos.js
@@ -1,10 +1,13 @@
 
 const productos = [];
-$.ajaxSetup({async: false});
-$.getJSON("../data.json", function(result, estado){
+const URLGET = "../data.json";
+//$.ajaxSetup({async: false});
+$.get("../data.json", function(result, estado){
     console.log(status);
-    for (const producto of result) {
-        productos.push(producto);
+    if(estado === "success"){
+        for (const producto of result) {
+            productos.push(producto);
+        }    
     }
 });
 
